b05a2112e6d64e234ce535e71e9794cd1fcdc05f
Liziane Tamm <lizianetamm@hotmail.com>
2021-09-24 14:08:47 -0300

    Update CashbackDAO.ts

--- src/model/dao/CashbackDAO.ts
+++ src/model/dao/CashbackDAO.ts
@@ -1,5 +1,6 @@
 import { db } from "../../db.config";
 import Cashback from "../entidade/cashback";
+import EntidadeDominio from "../entidade/entidade.model";
 import entidadeModel from "../entidade/entidade.model";
 import IDAO from "./IDAO";
 
@@ -21,7 +22,6 @@ export default class CashbackDAO implements IDAO {
 
     async alterar(entidade: entidadeModel): Promise<entidadeModel> {
         const cashback = entidade as Cashback
-        console.log ("alterar", cashback.idCliente)
         await db.query("UPDATE cashback SET valor=$1, fk_cliente=$2 WHERE id=$3",
         [
             cashback.valor,
@@ -34,11 +34,29 @@ export default class CashbackDAO implements IDAO {
     excluir(entidade: entidadeModel): boolean {
         throw new Error("Method not implemented.");
     }
-    consultar(): Promise<entidadeModel[]> {
-        throw new Error("Method not implemented.");
+    async consultar(): Promise<entidadeModel[]> {
+        let cashback = db.query("SELECT * FROM cashback");
+        let result: Array<EntidadeDominio> = [];
+
+        result = await cashback.then((dados) => {
+            return (result = dados.rows.map((cashback) => {
+                return cashback as Cashback;
+            }));
+        });
+
+        return result;
     }
-    consultarComId(entidade: entidadeModel): Promise<entidadeModel[]> {
-        throw new Error("Method not implemented.");
+    async consultarComId(entidade: entidadeModel): Promise<entidadeModel[]> {
+        const cashback = entidade as Cashback;
+        let cash = db.query("SELECT * FROM cashback WHERE id = $1", [cashback.id]);
+        let result: Array<EntidadeDominio> = [];
+
+        result = await cash.then((dados) => {
+            return (result = dados.rows.map((cashback) => {
+                return cashback;
+            }));
+        });
+        return result;
     }
 
 }
